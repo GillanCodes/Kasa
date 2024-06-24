af8eb482bdde8e2517acf6a216e81ceb5ebc92f4
gillancodes <gillancodes@laposte.net>
2024-06-24 13:00:34 +0200

    feat: added data

--- src/Components/Home/Gallery.jsx
+++ src/Components/Home/Gallery.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Card from '../../Modules/Card'
+import React, { useEffect, useState } from 'react'
+import Card from '../../Modules/Card';
+import data from "../../backend.json";
 
 export default function Gallery() {
-  return (
-    <div className='gallery'>
-        <div className="cards">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-        </div> 
-    </div>
-  )
+    return (
+        <div className='gallery'>
+            <div className="cards">
+                {data.map((card) => {
+                    return (
+                        <>
+                            <Card id={card.id} title={card.title} img={card.cover} />
+                        </>
+                    )
+                })}
+            </div> 
+        </div>
+    )
 }
