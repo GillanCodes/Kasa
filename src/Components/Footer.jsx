dddb43c79c464c1968bd4f902ef7b1c96d239689
gillancodes <gillancodes@laposte.net>
2024-06-24 13:10:51 +0200

    feat: added footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <img src="./logo_alt.svg" alt="logo" />
+        <p>&copy; Kasa. All rights reserved</p>
+    </footer>
+  )
+}
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../Components/Home/Home";
 import Navbar from "../Components/Navbar";
 import NotFound from "../Components/NotFound";
+import Footer from "../Components/Footer";
 
 export default function index()
 {
@@ -13,6 +14,7 @@ export default function index()
                 <Route path="/about" Component={Home} />
                 <Route path="*" Component={NotFound} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
