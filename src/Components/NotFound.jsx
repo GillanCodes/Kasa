a3644113423e6b398e5d787630502e05fa30c5c3
gillancodes <gillancodes@laposte.net>
2024-06-24 13:24:41 +0200

    feat: 404 page done.

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <div>404</div>
+    <div className='notfound'>
+      <div className="container">
+
+        <p className='nf_text'>404</p>
+        <p className='message'>Oups! La page que vous demandez n'existe pas.</p>
+
+        <a href="/">Retourner sur la page d’accueil</a>
+
+      </div>
+    </div>
   )
 }
