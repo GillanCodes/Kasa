2fa418fd5ea513bf794d1f147e6a0dd17c65d44c
gillancodes <gillancodes@laposte.net>
2024-06-25 11:17:40 +0200

    feat: click go to rental page

--- src/Modules/Card.jsx
+++ src/Modules/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card({id, title, img}) {
   return (
-    <div className='card' id={id}>
+    <div className='card' id={id} onClick={() => window.location = `/rental/${id}`}>
       <img src={img} alt={`${title}-${id}`} />
       <p>{title}</p>
     </div>
