cd755cb0850862390bb835f286edc12520e7d74c
gillancodes <gillancodes@laposte.net>
2024-06-26 16:11:34 +0200

    feat: working slideshow

--- src/Components/Rental/Slideshow.jsx
+++ src/Components/Rental/Slideshow.jsx
@@ -4,13 +4,23 @@ export default function Slideshow({ pictures }) {
 
     const [slide, setSlide] = useState(0);
 
+    const previus = () => {
+        setSlide(slide === 0 ? pictures.length - 1 : slide - 1);
+    }
+
+    const next = () => {
+        setSlide(slide === pictures.length - 1 ? 0 : slide + 1);
+    }
+
     return (
         <div className="slideshow">
+            <span id='prev' onClick={previus}>prev</span>
             {pictures.map((pic, index) => {
                 return (
                     <img src={pic} key={index} alt="rental_slide" className={index === slide ? "slide" : "slide is-hidden"} />
                 )
             })}
+            <span id='next' onClick={next}>next</span>
         </div>
     )
 }
