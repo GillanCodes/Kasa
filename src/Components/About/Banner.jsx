a70ab6f4c00fba853d157d64470d83f377cb964b
gillancodes <gillancodes@laposte.net>
2024-06-27 10:13:04 +0200

    feat: about page done

---
+++ src/Components/About/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div className='banner'>
+        <img className='image' src="./bg_about.png" alt="background_home" />
+    </div>
+  )
+}
