22842a886e33185ca7936417d516eb6979007858
gillancodes <gillancodes@laposte.net>
2024-07-08 12:03:45 +0200

    refactor: added comments

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -10,13 +10,17 @@ export default function index()
 {
     return (
         <BrowserRouter>
+            {/* Navbar on all pages */}
             <Navbar />
+            {/* Route to display right components */}
             <Routes>
                 <Route path="/" Component={Home} />
                 <Route path="/about" Component={About} />
                 <Route path="/rental/:id" Component={Rental} />
+                {/* if not routes before, display 404 error */}
                 <Route path="*" Component={NotFound} />
             </Routes>
+            {/* Footer on all pages */}
             <Footer />
         </BrowserRouter>
     )
