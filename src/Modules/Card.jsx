bf8712bb755c5197baf6cea83995ea5bd608cfb1
gillancodes <gillancodes@laposte.net>
2024-07-08 12:02:33 +0200

    refactor: added comments

--- src/Modules/Card.jsx
+++ src/Modules/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Card({id, title, img}) {
+export default function Card({id, title, img}) { // props get an id, title and imgUrl
   return (
+    // We set onClick the location to "rent/id"
     <div className='card' id={id} onClick={() => window.location = `/rental/${id}`}>
-      <img src={img} alt={`${title}-${id}`} />
+      {/* Content with image and title */}
+      <img src={img} alt={`${title}-${id}`} /> 
       <p>{title}</p>
     </div>
   )
--- src/Modules/Dropdown.jsx
+++ src/Modules/Dropdown.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
-export default function Dropdown({type, title, data}) {
+export default function Dropdown({type, title, data}) { // We get a type, title and data
 
+    // By defaut the dropdown is close
     const [isOpen, setIsOpen] = useState(false);
 
+    // set state to the opposite state
     const handleOpen = () => {
         setIsOpen(!isOpen)
     }
@@ -11,13 +13,16 @@ export default function Dropdown({type, title, data}) {
     return (
         <div className='dropdown-container'>
             
-            <div className='dropdown-head' onClick={handleOpen}>
+            {/* Onclick head, we switch state */}
+            <div className='dropdown-head' onClick={handleOpen}> 
                 <p>{title}</p>
+                {/* The "chevron" swith side (up = open | down = close) */}
                 {isOpen ? <i class="fa-solid fa-chevron-down"></i> : <i class="fa-solid fa-chevron-up"></i> }
             </div>
 
-
+                {/* If body is open, display body content */}
                 <div className={`dropdown-body is-${isOpen ? "open" : "close"}`}>
+                    {/* Type array = a list */}
                     {type === "array" && (
                         <>
                             <ul>
@@ -30,12 +35,14 @@ export default function Dropdown({type, title, data}) {
                         </>
                     )}
 
+                    {/* Type string just display a paragraph */}
                     {type === "string" && (
                         <>
                             <p>{data}</p>
                         </>
                     )}
 
+                    {/* If no type , just display error */}
                     {type !== "string" && type !== "array" && (
                         <p>Invalid_type_props : Type prop should be ethier "string" or "array"</p>
                     )}
