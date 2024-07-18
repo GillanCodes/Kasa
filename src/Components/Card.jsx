7b2765d1c7261b8a350429abb3247058131e47db
gillancodes <gillancodes@laposte.net>
2024-07-18 19:37:34 +0200

    fix: card style

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function Card({id, title, img}) { // props get an id, title and imgUrl
   return (
-    <Link to={`/rental/${id}`} id={id}>
+    <Link to={`/rental/${id}`} id={id} className='card'>
       {/* // We set onClick the location to "rent/id" */}
-      <div className='card'>
         <div className='gradient'></div>
         {/* Content with image and title */}
         <img src={img} alt={`${title}-${id}`} /> 
         <p>{title}</p>
-      </div>
     </Link>
   )
 }
